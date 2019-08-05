b760050da86e29bcbc4578f460b79d26c7903c0b
Christopher Brown <io@henrian.com>
2019-08-05 12:08:46 -0400

    Improve drop_uninformative_columns test

--- test/test_pandas.py
+++ test/test_pandas.py
@@ -15,5 +15,12 @@ def test_drop_uninformative_columns(n: int = 10):
         'all_but_one':     np.concatenate([np.repeat(100, n - 1), [99]]),
         'all_but_one_2':   np.concatenate([[-1], np.repeat(0, n - 1)]),
     })
-    for column in drop_uninformative_columns(df).columns:
-        assert not column.startswith('repeated_'), f'Column {column} should have been dropped'
+    original_columns = set(df.columns)
+    uninformative_columns = {column for column in original_columns
+                             if column.startswith('repeated_')}
+    informative_columns = original_columns - uninformative_columns
+    # run function and compare result to original
+    df = drop_uninformative_columns(df)
+    resulting_columns = set(df.columns)
+    # check that informative columns were not dropped and that uninformative columns were
+    assert resulting_columns == informative_columns
