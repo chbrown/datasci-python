0e3cdbc9bea3e771d9831a7f1b2ec1a8ed1b8059
Christopher Brown <io@henrian.com>
2019-10-17 11:54:54 -0400

    Split out identification logic from drop operations

--- datasci/pandas/__init__.py
+++ datasci/pandas/__init__.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from typing import Any, Iterator, Tuple
 import logging
 import numpy as np
 import pandas as pd
@@ -13,9 +14,10 @@ def drop_na_columns(df: pd.DataFrame, inplace=False) -> pd.DataFrame:
     return df.dropna(axis='columns', how='all', inplace=inplace)
 
 
-def drop_uninformative_columns(df: pd.DataFrame) -> pd.DataFrame:
+def uninformative_columns(df: pd.DataFrame) -> Iterator[Tuple[str, Any]]:
     """
-    Drop columns from df where values in all cells are identical.
+    List columns of df where values in all cells are identical,
+    along with that identical value.
     """
     # TODO: support DataFrames where df.columns is a MultiIndex
     for column in df.columns:
@@ -29,11 +31,42 @@ def drop_uninformative_columns(df: pd.DataFrame) -> pd.DataFrame:
         # nan is a special case, since np.nan != np.nan
         if series.dtype == np.float and np.isnan(exemplar):
             if all(np.isnan(item) for item in series_iter):
-                logger.debug('Dropping column %r from DataFrame (every value is nan)', column)
-                df = df.drop(column, axis='columns')
+                yield column, exemplar
         elif all(item == exemplar for item in series_iter):
-            logger.debug('Dropping column %r from DataFrame (every value = %r)', column, exemplar)
-            df = df.drop(column, axis='columns')
+            yield column, exemplar
+
+
+def drop_uninformative_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Drop columns from df where values in all cells are identical.
+    """
+    for column, value in uninformative_columns(df):
+        logger.debug(
+            'Dropping column %r from DataFrame (every value %s %r)',
+            column,
+            'is' if isinstance(value, float) and np.isnan(value) else '=',
+            value,
+        )
+        df = df.drop(column, axis='columns')
+    return df
+
+
+def duplicate_columns(df: pd.DataFrame) -> Iterator[Tuple[str, str]]:
+    """
+    List columns of df where values in all cells are identical to those in a preceding column,
+    along with the preceding column having identical values.
+    """
+    columns = set(df.columns)
+    for column1, column2 in combinations(df.columns, 2):
+        if column1 not in columns or column2 not in columns:
+            continue
+        series1 = df[column1]
+        series2 = df[column2]
+        # convert dtypes to strings since numpy raises "TypeError: data type not understood"
+        # when comparing to pandas's dtypes extensions
+        if str(series1.dtype) == str(series2.dtype) and all(series1 == series2):
+            yield column2, column1
+            columns.remove(column2)
     return df
 
 
@@ -41,14 +74,11 @@ def drop_duplicate_columns(df: pd.DataFrame) -> pd.DataFrame:
     """
     Drop columns from df where values in all cells are identical to those in a preceding column.
     """
-    for column1, column2 in combinations(df.columns, 2):
-        if column1 not in df or column2 not in df:
-            continue
-        series1 = df[column1]
-        series2 = df[column2]
-        # convert dtypes to strings since numpy raises "TypeError: data type not understood"
-        # when comparing to pandas's dtypes extensions
-        if str(series1.dtype) == str(series2.dtype) and all(series1 == series2):
-            logger.debug('Dropping column %r from DataFrame (duplicate of %r)', column2, column1)
-            df = df.drop(column2, axis='columns')
+    for duplicate_column, original_column in duplicate_columns(df):
+        logger.debug(
+            'Dropping column %r from DataFrame (duplicate of %r)',
+            duplicate_column,
+            original_column,
+        )
+        df = df.drop(duplicate_column, axis='columns')
     return df
